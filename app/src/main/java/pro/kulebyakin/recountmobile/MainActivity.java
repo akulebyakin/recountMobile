c1fda13506f0908fb915a3d32259ff5b72286045
Andrey Kulebyakin <wowand@gmail.com>
2019-05-30 12:00:51 +0300

    Добавил красивые подсказки в поля

--- app/src/main/java/pro/kulebyakin/recountmobile/MainActivity.java
+++ app/src/main/java/pro/kulebyakin/recountmobile/MainActivity.java
@@ -3,6 +3,8 @@ package pro.kulebyakin.recountmobile;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.graphics.Rect;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,6 +19,21 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextInputLayout whiteSalInputTil;
+    TextInputLayout blackSalInputTil;
+    TextInputLayout weekendsInputTil;
+    TextInputLayout mileageInputTil;
+    TextInputLayout fuelCostInputTil;
+    TextInputLayout consumptionInputTil;
+    TextInputEditText whiteSalInput;
+    TextInputEditText blackSalInput;
+    TextInputEditText weekendsInput;
+    TextInputEditText mileageInput;
+    TextInputEditText fuelCostInput;
+    TextInputEditText consumptionInput;
+    TextView result;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,20 +42,21 @@ public class MainActivity extends AppCompatActivity {
         // Запрет смены ориентации
         //setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        EditText consInp = findViewById(R.id.consumption);
-//        consInp.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-//            @Override
-//            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-//                if (event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
-//                    printResult();
-//                    Log.i("INFO", "Ok");
-//                    return true;
-//                } else {
-//                    return false;
-//                }
-//            }
-//        });
-        consInp.setOnKeyListener(new View.OnKeyListener() {
+        whiteSalInputTil = findViewById(R.id.whiteSalaryTil);
+        blackSalInputTil = findViewById(R.id.blackSalaryTil);
+        weekendsInputTil = findViewById(R.id.weekendsTil);
+        mileageInputTil = findViewById(R.id.mileageTil);
+        fuelCostInputTil = findViewById(R.id.fuelCostTil);
+        consumptionInputTil = findViewById(R.id.consumptionTil);
+        whiteSalInput = whiteSalInputTil.findViewById(R.id.whiteSalary);
+        blackSalInput = blackSalInputTil.findViewById(R.id.blackSalary);
+        weekendsInput = weekendsInputTil.findViewById(R.id.weekends);
+        mileageInput = mileageInputTil.findViewById(R.id.mileage);
+        fuelCostInput = fuelCostInputTil.findViewById(R.id.fuelCost);
+        consumptionInput = consumptionInputTil.findViewById(R.id.consumption);
+        result = findViewById(R.id.result);
+
+        consumptionInput.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 if (keyCode == 66) {
@@ -67,14 +85,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void printResult() {
-        EditText whiteSalInput = findViewById(R.id.whiteSalary);
-        EditText blackSalInput = findViewById(R.id.blackSalary);
-        EditText weekendsInput = findViewById(R.id.weekends);
-        EditText mileageInput = findViewById(R.id.mileage);
-        EditText fuelCostInput = findViewById(R.id.fuelCost);
-        EditText consumptionInput = findViewById(R.id.consumption);
-        TextView result = findViewById(R.id.result);
-
         double tax = 0.87; //Налог 13%
         double valPayToWeekend = 2500; //Оплата выходного дня
         double valByKM = 8.5; //Оплата амортизации
@@ -120,14 +130,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void clearAll(View view) {
-        EditText whiteSalInput = findViewById(R.id.whiteSalary);
-        EditText blackSalInput = findViewById(R.id.blackSalary);
-        EditText weekendsInput = findViewById(R.id.weekends);
-        EditText mileageInput = findViewById(R.id.mileage);
-        EditText fuelCostInput = findViewById(R.id.fuelCost);
-        EditText consumptionInput = findViewById(R.id.consumption);
-        TextView result = findViewById(R.id.result);
-
         whiteSalInput.setText(null);
         blackSalInput.setText(null);
         weekendsInput.setText(null);
